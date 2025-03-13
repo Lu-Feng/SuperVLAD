dea6b2eb6b7986bb4ef76038e5dcdcca2fe330eb
Feng Lu <1600925872@qq.com>
2025-03-13 15:40:40 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
 # SuperVLAD
+This is the official repository for the NeurIPS 2024 paper "[SuperVLAD: Compact and Robust Image Descriptors for Visual Place Recognition](https://proceedings.neurips.cc/paper_files/paper/2024/hash/0b135d408253205ba501d55c6539bfc7-Abstract-Conference.html)".
+
+<img src="image/architecture.png" width="800px">
+
+## Getting Started
+
+This repo follows the framework of [GSV-Cities](https://github.com/amaralibey/gsv-cities) for training, and the [Visual Geo-localization Benchmark](https://github.com/gmberton/deep-visual-geo-localization-benchmark) for evaluation. You can download the GSV-Cities datasets [HERE](https://www.kaggle.com/datasets/amaralibey/gsv-cities), and refer to [VPR-datasets-downloader](https://github.com/gmberton/VPR-datasets-downloader) to prepare test datasets.
+
+The test dataset should be organized in a directory tree as such:
+
+```
+├── datasets_vg
+    └── datasets
+        └── pitts30k
+            └── images
+                ├── train
+                │   ├── database
+                │   └── queries
+                ├── val
+                │   ├── database
+                │   └── queries
+                └── test
+                    ├── database
+                    └── queries
+```
+
+Before training, you should download the pre-trained foundation model DINOv2(ViT-B/14) [HERE](https://dl.fbaipublicfiles.com/dinov2/dinov2_vitb14/dinov2_vitb14_pretrain.pth).
+
+## Train
+```
+python3 train.py --eval_datasets_folder=/path/to/your/datasets_vg/datasets --eval_dataset_name=pitts30k --foundation_model_path=/path/to/pre-trained/dinov2_vitb14_pretrain.pth --backbone=dino --supervlad_clusters=4 --crossimage_encoder --patience=3 --lr=0.00005 --epochs_num=20 --train_batch_size=120 --freeze_te 8
+```
+
+## Test
+
+To evaluate the trained model:
+
+```
+python3 eval.py --eval_datasets_folder=/path/to/your/datasets_vg/datasets --eval_dataset_name=pitts30k --resume=/path/to/trained/model/SuperVLAD.pth --backbone=dino --supervlad_clusters=4 --crossimage_encoder
+```
+
+## SuperVLAD without cross-image encoder
+
+Remove parameter `--crossimage_encoder` to run the SuperVLAD without cross-image encoder.
+
+## 1-cluster VLAD
+
+Set `--supervlad_clusters=1` and `--ghost_clusters=2` to run the 1-cluster VLAD.
+
+For example,
+
+```
+python3 eval.py --eval_datasets_folder=/path/to/your/datasets_vg/datasets --eval_dataset_name=pitts30k --resume=/path/to/trained/model/SuperVLAD.pth --backbone=dino --supervlad_clusters=1 --ghost_clusters=2
+```
+
+## Trained Model
+
+<table style="margin: auto">
+  <thead>
+    <tr>
+      <th>model</th>
+      <th>cross-image<br />encoder</th>
+      <th>download</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>SuperVLAD</td>
+      <td align="center">:x:</td>
+      <td><a href="`https://drive.google.com/file/d/1wRkUO4E8s5hNRNNIWcuA8RUvlGob3Tbf/view?usp=drive_link`">LINK</a></td>
+    </tr>
+    <tr>
+      <td>SuperVLAD</td>
+      <td align="center">:white_check_mark:</td>
+      <td><a href="`https://drive.google.com/file/d/1yomnWGTJko6nf3F2Ju6RWsLhP2EG82tL/view?usp=drive_link`">LINK</a></td>
+    </tr>
+  </tbody>
+</table>
+
+## Acknowledgements
+
+Parts of this repo are inspired by the following repositories:
+
+[GSV-Cities](https://github.com/amaralibey/gsv-cities)
+
+[Visual Geo-localization Benchmark](https://github.com/gmberton/deep-visual-geo-localization-benchmark)
+
+[DINOv2](https://github.com/facebookresearch/dinov2)
+
+## Citation
+
+If you find this repo useful for your research, please cite the paper
+
+```
+@inproceedings{lu2024supervlad,
+  title={SuperVLAD: Compact and Robust Image Descriptors for Visual Place Recognition},
+  author={Lu, Feng and Zhang, Xinyao and Ye, Canming and Dong, Shuting and Zhang, Lijun and Lan, Xiangyuan and Yuan, Chun},
+  booktitle={Advances in Neural Information Processing Systems},
+  volume={37},
+  pages={5789--5816},
+  year={2024}
+}
+```
---
+++ train.py
@@ -0,0 +1,253 @@
+
+import math
+import torch
+import logging
+import numpy as np
+from tqdm import tqdm,trange
+import torch.nn as nn
+import multiprocessing
+from os.path import join
+from datetime import datetime
+import torchvision.transforms as transforms
+from torch.utils.data.dataloader import DataLoader
+torch.backends.cudnn.benchmark= True  # Provides a speedup
+
+import util
+import test
+import parser
+import commons
+import datasets_ws
+from model import network
+from model.sync_batchnorm import convert_model
+
+import warnings
+warnings.filterwarnings("ignore")
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+#### Initial setup: parser, logging...
+args = parser.parse_arguments()
+start_time = datetime.now()
+args.save_dir = join("logs", args.save_dir, start_time.strftime('%Y-%m-%d_%H-%M-%S'))
+commons.setup_logging(args.save_dir)
+commons.make_deterministic(args.seed)
+logging.info(f"Arguments: {args}")
+logging.info(f"The outputs are being saved in {args.save_dir}")
+logging.info(f"Using {torch.cuda.device_count()} GPUs and {multiprocessing.cpu_count()} CPUs")
+
+#### Creation of Datasets
+logging.debug(f"Loading dataset {args.eval_dataset_name} from folder {args.eval_datasets_folder}")
+
+triplets_ds = datasets_ws.TripletsDataset(args, args.eval_datasets_folder, args.eval_dataset_name, "train", args.negs_num_per_query)
+logging.info(f"Train query set: {triplets_ds}")
+
+val_ds = datasets_ws.BaseDataset(args, args.eval_datasets_folder, args.eval_dataset_name, "val")
+logging.info(f"Val set: {val_ds}")
+
+test_ds = datasets_ws.BaseDataset(args, args.eval_datasets_folder, args.eval_dataset_name, "test")
+logging.info(f"Test set: {test_ds}")
+
+#### Initialize model
+model = network.SuperVLADModel(args, pretrained_foundation = True, foundation_model_path = args.foundation_model_path)
+
+model = model.to(args.device)
+
+# Initialize (the conv layer in) SuperVLAD layer
+if not args.resume:
+    triplets_ds.is_inference = True
+    model.aggregation.initialize_supervlad_layer(args, triplets_ds, model)
+args.features_dim *= args.supervlad_clusters
+
+model = torch.nn.DataParallel(model)
+
+#### Setup Optimizer and Loss
+if args.optim == "adam":
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    if args.crossimage_encoder:
+        optim_encoder = torch.optim.Adam(model.module.encoder.parameters(), lr=args.lr_encoder)
+    # optim_conv = torch.optim.Adam(model.module.conv.parameters(), lr=args.lr_encoder)
+elif args.optim == "sgd":
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=0.001)
+elif args.optim == "adamw":
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=9.5e-9)
+
+#### Resume model, optimizer, and other training parameters
+if args.resume:
+    model, optimizer, best_r5, start_epoch_num, not_improved_num = util.resume_train(args, model, optimizer)
+    logging.info(f"Resuming from epoch {start_epoch_num} with best recall@5 {best_r5:.1f}")
+else:
+    best_r5 = start_epoch_num = not_improved_num = 0
+
+logging.info(f"Output dimension of the model is {args.features_dim}")
+
+if torch.cuda.device_count() >= 2:
+    # When using more than 1GPU, use sync_batchnorm for torch.nn.DataParallel
+    model = convert_model(model)
+    model = model.cuda()
+
+from torch.utils.data.dataloader import DataLoader
+from torchvision import transforms as T
+from dataloaders.train.GSVCitiesDataset import GSVCitiesDataset
+
+IMAGENET_MEAN_STD = {'mean': [0.485, 0.456, 0.406], 
+                     'std': [0.229, 0.224, 0.225]}
+
+TRAIN_CITIES = [
+    'Bangkok',
+    'BuenosAires',
+    'LosAngeles',
+    'MexicoCity',
+    'OSL', # refers to Oslo
+    'Rome',
+    'Barcelona',
+    'Chicago',
+    'Madrid',
+    'Miami',
+    'Phoenix',
+    'TRT', # refers to Toronto
+    'Boston',
+    'Lisbon',
+    'Medellin',
+    'Minneapolis',
+    'PRG', # refers to Prague
+    'WashingtonDC',
+    'Brussels',
+    'London',
+    'Melbourne',
+    'Osaka',
+    'PRS', # refers to Paris
+]
+
+batch_size=args.train_batch_size #32
+img_per_place=4
+min_img_per_place=4
+shuffle_all=False
+image_size=(224, 224)#(256,256)#(320,320)#
+num_workers=4
+cities=TRAIN_CITIES
+mean_std=IMAGENET_MEAN_STD
+random_sample_from_each_place=True
+
+mean_dataset = mean_std['mean']
+std_dataset = mean_std['std']
+train_transform = T.Compose([
+    T.Resize(image_size, interpolation=T.InterpolationMode.BILINEAR),
+    T.RandAugment(num_ops=3, interpolation=T.InterpolationMode.BILINEAR),
+    T.ToTensor(),
+    T.Normalize(mean=mean_dataset, std=std_dataset),
+])
+
+train_loader_config = {
+    'batch_size': batch_size,
+    'num_workers': num_workers,
+    'drop_last': False,
+    'pin_memory': True,
+    'shuffle': shuffle_all}
+
+train_dataset = GSVCitiesDataset(
+            cities=cities,
+            img_per_place=img_per_place,
+            min_img_per_place=min_img_per_place,
+            random_sample_from_each_place=random_sample_from_each_place,
+            transform=train_transform)
+
+from pytorch_metric_learning import losses, miners
+from pytorch_metric_learning.distances import CosineSimilarity, DotProductSimilarity
+loss_fn = losses.MultiSimilarityLoss(alpha=1.0, beta=50, base=0.0, distance=DotProductSimilarity())
+miner = miners.MultiSimilarityMiner(epsilon=0.1, distance=CosineSimilarity())
+#  The loss function call (this method will be called at each training iteration)
+def loss_function(descriptors, labels):
+    # we mine the pairs/triplets if there is an online mining strategy
+    if miner is not None:
+        miner_outputs = miner(descriptors, labels)
+        loss = loss_fn(descriptors, labels, miner_outputs)
+
+        # calculate the % of trivial pairs/triplets 
+        # which do not contribute in the loss value
+        nb_samples = descriptors.shape[0]
+        nb_mined = len(set(miner_outputs[0].detach().cpu().numpy()))
+        batch_acc = 1.0 - (nb_mined/nb_samples)
+
+    else: # no online mining
+        loss = loss_fn(descriptors, labels)
+        batch_acc = 0.0
+    return loss
+
+#### Training loop
+ds = DataLoader(dataset=train_dataset, **train_loader_config)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=len(ds)*3, gamma=0.5, last_epoch=-1)
+# scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1, end_factor=0.15, total_iters=4000)
+for epoch_num in range(start_epoch_num, args.epochs_num):
+    logging.info(f"Start training epoch: {epoch_num:02d}")
+    epoch_start_time = datetime.now()
+    epoch_losses = np.zeros((0,1), dtype=np.float32)
+        
+    model = model.train()
+    epoch_losses=[]
+    for images, place_id in tqdm(ds):      
+        BS, N, ch, h, w = images.shape
+        # reshape places and labels
+        images = images.view(BS*N, ch, h, w)
+        labels = place_id.view(-1)
+
+        descriptors = model(images.to(args.device))
+        descriptors = descriptors.cuda()
+        loss = loss_function(descriptors, labels) # Call the loss_function we defined above     
+        del descriptors
+        
+        optimizer.zero_grad()
+        loss.backward()
+
+        if args.crossimage_encoder:
+            for p in model.module.encoder.parameters():
+                p.requires_grad = True
+            optim_encoder.step() 
+            for p in model.module.encoder.parameters():
+                p.requires_grad = False 
+
+        optimizer.step()
+        scheduler.step()       
+        
+        # Keep track of all losses by appending them to epoch_losses
+        batch_loss = loss.item()
+        epoch_losses = np.append(epoch_losses, batch_loss)
+        del loss
+
+    
+    logging.info(f"Finished epoch {epoch_num:02d} in {str(datetime.now() - epoch_start_time)[:-7]}, "
+                 f"average epoch triplet loss = {epoch_losses.mean():.4f}")
+    
+    # Compute recalls on validation set
+    recalls, recalls_str = test.test(args, val_ds, model)
+    logging.info(f"Recalls on val set {val_ds}: {recalls_str}")
+    
+    is_best = recalls[0]+recalls[1] > best_r5
+    
+    # Save checkpoint, which contains all training parameters
+    util.save_checkpoint(args, {"epoch_num": epoch_num, "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(), "recalls": recalls, "best_r5": best_r5,
+        "not_improved_num": not_improved_num
+    }, is_best, filename="last_model.pth")
+    
+    # If recall@5 did not improve for "many" epochs, stop training
+    if is_best:
+        logging.info(f"Improved: previous best R@5 = {best_r5:.1f}, current R@5 = {(recalls[0]+recalls[1]):.1f}")
+        best_r5 = (recalls[0]+recalls[1])
+        not_improved_num = 0
+    else:
+        not_improved_num += 1
+        logging.info(f"Not improved: {not_improved_num} / {args.patience}: best R@5 = {best_r5:.1f}, current R@5 = {(recalls[0]+recalls[1]):.1f}")
+        if not_improved_num >= args.patience:
+            logging.info(f"Performance did not improve for {not_improved_num} epochs. Stop training.")
+            break
+
+
+logging.info(f"Best R@5: {best_r5:.1f}")
+logging.info(f"Trained for {epoch_num+1:02d} epochs, in total in {str(datetime.now() - start_time)[:-7]}")
+
+#### Test best model on test set
+best_model_state_dict = torch.load(join(args.save_dir, "best_model.pth"))["model_state_dict"]
+model.load_state_dict(best_model_state_dict)
+
+recalls, recalls_str = test.test(args, test_ds, model, test_method=args.test_method)
+logging.info(f"Recalls on {test_ds}: {recalls_str}")
+
